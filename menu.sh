ce7c4d2af612e1d18bf481ffe2209f7fd9482093
Atila <atilajmsousa@gmail.com>
2026-03-24 13:36:31 -0300

    bugfix

--- menu.sh
+++ menu.sh
@@ -1,31 +1,51 @@
 #!/bin/bash
 
+# Variáveis Globais
 export FILENAME="alumnos"
-
 ENTORNO="$HOME/EPNro1"
 ARCHIVO_SALIDA="$ENTORNO/salida/${FILENAME}.txt"
 SCRIPT_CONSOLIDAR="$ENTORNO/consolidar.sh"
 
+crear_entorno() {
+    local ar_ejemplo="$ENTORNO/entrada/data.txt"
 
-borrar_entorno() {
-    echo "Borrando entorno..."
-    pkill -f "consolidar.sh"
-    rm -rf "$HOME/EPNro1"
-    echo "Entorno eliminado y procesos finalizados."
+    if [[ ! -d "$ENTORNO" ]]; then
+        echo "Creando el entorno de trabajo en: $ENTORNO"
+        
+        mkdir -p "$ENTORNO"/{entrada,salida,procesado}
+        touch "$ARCHIVO_SALIDA"
+
+        if [ -f "consolidar.sh" ]; then
+            cp "consolidar.sh" "$SCRIPT_CONSOLIDAR"
+            chmod 700 "$SCRIPT_CONSOLIDAR"
+        else
+            echo "Aviso: consolidar.sh original no encontrado para copiar."
+        fi
+
+        echo -e "122332 Juan Lopez jlopez@fi.uba.ar 8\n100998 Pedro Valdéz pvaldez@fi.uba.ar 5\n89032 Carla Simone csimone@fi.uba.ar 7\n77542 Franco Lomba flomba@fi.uba.ar 10\n100223 Juana Pola jpola@fi.uba.ar 4\n122435 Lucia Fernandez lfernandez@fi.uba.ar 9" > "$ar_ejemplo"
+        
+        echo "Entorno creado y listo."
+    else
+        echo "El entorno ya existe en $ENTORNO. No se realizaron cambios."
+    fi
 }
 
-crear_entorno() {
-    echo "Opcion seleccionada: Crear entorno"
-    echo "Creando entorno en $ENTORNO..."
-    
-    mkdir -p "$ENTORNO"/{entrada,salida,procesado}
-    touch "$ARCHIVO_SALIDA"
-    if [ ! -f "$SCRIPT_CONSOLIDAR" ]; then
-        cp "consolidar.sh" "$SCRIPT_CONSOLIDAR"
-        chmod +x "$SCRIPT_CONSOLIDAR"
+borrar_entorno() {
+    echo "Iniciando limpieza del entorno..."
+
+    # 1. Mata processos que estejam rodando o consolidar.sh
+    # O 2>/dev/null evita mensagens de erro caso não haja processos ativos
+    pkill -f "consolidar.sh" 2>/dev/null
+
+    # 2. Remove o diretório inteiro de forma recursiva
+    if [[ -d "$ENTORNO" ]]; then
+        rm -rf "$ENTORNO"
+        echo "Directorio $ENTORNO y subcarpetas eliminados."
+    else
+        echo "No se encontró el directorio $ENTORNO para borrar."
     fi
 
-    echo "Entorno creado."
+    echo "Limpieza finalizada."
 }
 
 correr_proceso() {
@@ -83,7 +103,6 @@ imprimir_menu() {
 main() {
     if [ "$1" == "-d" ]; then
         borrar_entorno
-        exit 0
     fi
 
     while true; do
@@ -114,12 +133,7 @@ main() {
                 echo "Opción no válida. Intente de nuevo."
                 ;;
         esac
-        
-        echo ""
-        echo "Presione Enter para continuar..."
-        read CONTINUAR
-        clear
     done
 }
 
-main
+main "$@"
